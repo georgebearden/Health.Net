5011632e9c085659c3dd2df3729089c1cc24052e
George Bearden <george@goerden.com>
2015-12-29 17:40:16 -0500

    Manually update splat to the latest version to get reactiveui working.

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Health.Net.ViewModels;
+
+namespace Health.Net
+{
+  public class MainWindowViewModel : ReactiveObject
+  {
+
+    AddFoodViewModel addFoodViewModel;
+    public AddFoodViewModel AddFoodViewModel
+    {
+      get { return addFoodViewModel; }
+      set { this.RaiseAndSetIfChanged(ref addFoodViewModel, value); }
+    }
+  }
+}
